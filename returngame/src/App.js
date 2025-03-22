67427977fd438840efce5a1cda4f3d094e4c3825
선욱 <80817426+nasu0604@users.noreply.github.com>
2025-03-23 03:04:41 +0900

    Create Project

---
+++ returngame/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Welcome to my Game Development Site!</h1>
+      <p>Start building your game ideas here.</p>
+    </div>
+  );
+}
+
+export default App;
